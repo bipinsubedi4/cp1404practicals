7f34228e17f7de1b7ee771fae9b97734a737a7e0
Bipin <beepin567@gmail.com>
2024-11-03 07:09:24 +1000

    Get the programming language

---
+++ prac_06/languages.py
@@ -0,0 +1,25 @@
+"""
+languages.py
+Estimated Time: 10 minutes
+Start Time: 7:07 AM
+End Time: 7:16 AM, 9  minutes
+
+This program tests the ProgrammingLanguage class by creating several instances
+and printing them to verify the __str__ method's output.
+"""
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    java = ProgrammingLanguage("Java", "Static", True, 1995)
+    c_plus_plus = ProgrammingLanguage("C++", "Static", True, 1995)
+    languages = [python, ruby, visual_basic, java, c_plus_plus]
+    for language in languages:
+        print(language)
+        print(language.is_dynamic())
+
+
+main()
