96f08a3b69bab8bbf84f499ce80c14e01b400903
Bipin <beepin567@gmail.com>
2024-11-16 13:24:51 +1000

    Add the greet function

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,13 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        """Handles the Greet button click event."""
+        name = self.root.ids.input_name.text
+        if name.strip():
+            self.root.ids.output_label.text = f"Hello {name}"
+        else:
+            self.root.ids.output_label.text = "Hello!"
+
 
 BoxLayoutDemo().run()
