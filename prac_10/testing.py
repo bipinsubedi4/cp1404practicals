74dd590d2cdd9f5bd8c595b347d458adbe946558
Bipin <beepin567@gmail.com>
2024-12-01 10:20:48 +1000

    Fix the failing is_long_word function.

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -5,3 +5,16 @@ from car import Car
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
     return " ".join([s] * n)
+
+
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
