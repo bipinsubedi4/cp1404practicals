3f783825615f3eac61a0d481512f9e7601effa08
Bipin <beepin567@gmail.com>
2024-12-01 10:46:07 +1000

    define main function

---
+++ prac_10/wiki.py
@@ -0,0 +1,8 @@
+import wikipedia
+import warnings
+
+# Suppress BeautifulSoup warnings for cleaner output
+warnings.filterwarnings("ignore", category=UserWarning, module="wikipedia")
+
+
+def main():
